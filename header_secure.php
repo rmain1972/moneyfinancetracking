ed5dd9375e1f89df034053b03a2b0cf19d503189
Ralph MAin <rmain1972@live.com>
2018-10-15 11:11:19 -0500

    deleted Admin/Messages from header/header_secure.php

--- header_secure.php
+++ header_secure.php
@@ -31,10 +31,7 @@ if ($loggedin == 0) {
     print '<o><a href="./login.html">Login</a></p>';
 } else {
     print '<p><a href="./logout.php">Logout</a><BR>You are logged in as <strong>' . $username . '</strong></p>';
-    print '<p>Access the <a href="admin.php">Admin Page</a></p>';
-    print '<p>Access the <a href="viewmessages.php">View Messages</a></p>';
 }
-
             
 ?>
 </div>
